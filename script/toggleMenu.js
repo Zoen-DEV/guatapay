f4c891a5ef152a72fc1803cf7584767fdd9f1ca4
Enzo Holgado <enzoholgadodev@gmail.com>
2023-06-26 15:25:09 -0500

    implementing the responsive menu

--- script/toggleMenu.js
+++ script/toggleMenu.js
@@ -1,11 +1,14 @@
 const toggleMenuBtn = document.querySelector(".menu");
 const menu = document.querySelector(".menu-content");
-const toggleSolutions = document.querySelector(".link2-responsive");
+const toggleSolutions = document.querySelector(".link-responsive");
 const menuSolutions = document.querySelector(".menu-soluciones-responsive");
+const menuClose = document.querySelector(".menu-close");
 
 toggleMenuBtn.addEventListener("click", (e) => {
   if (menu.classList.contains("show-menu-content")) {
     menu.classList.remove("show-menu-content");
+    menuSolutions.classList.remove("show-menu-soluciones");
+    menu.classList.remove("extend-menu-content");
   } else {
     menu.classList.add("show-menu-content");
   }
@@ -14,7 +17,15 @@ toggleMenuBtn.addEventListener("click", (e) => {
 toggleSolutions.addEventListener("click", (e) => {
   if (menuSolutions.classList.contains("show-menu-soluciones")) {
     menuSolutions.classList.remove("show-menu-soluciones");
+    menu.classList.remove("extend-menu-content");
   } else {
     menuSolutions.classList.add("show-menu-soluciones");
+    menu.classList.add("extend-menu-content");
   }
+});
+
+menuClose.addEventListener("click", (e) => {
+    menu.classList.remove("show-menu-content");
+    menuSolutions.classList.remove("show-menu-soluciones");
+    menu.classList.remove("extend-menu-content");
 });

0d43393a4853840afaa372d914b77b7b83b8eb12
Enzo Holgado <enzoholgadodev@gmail.com>
2023-06-26 15:50:46 -0500

    feat: update the hamburger menu

--- script/toggleMenu.js
+++ script/toggleMenu.js
@@ -3,6 +3,7 @@ const menu = document.querySelector(".menu-content");
 const toggleSolutions = document.querySelector(".link-responsive");
 const menuSolutions = document.querySelector(".menu-soluciones-responsive");
 const menuClose = document.querySelector(".menu-close");
+const solutionIcon = document.querySelector(".icon-right-responsive")
 
 toggleMenuBtn.addEventListener("click", (e) => {
   if (menu.classList.contains("show-menu-content")) {
@@ -18,9 +19,11 @@ toggleSolutions.addEventListener("click", (e) => {
   if (menuSolutions.classList.contains("show-menu-soluciones")) {
     menuSolutions.classList.remove("show-menu-soluciones");
     menu.classList.remove("extend-menu-content");
+    solutionIcon.classList.remove("rotate-solution-icon")
   } else {
     menuSolutions.classList.add("show-menu-soluciones");
     menu.classList.add("extend-menu-content");
+    solutionIcon.classList.add("rotate-solution-icon")
   }
 });
 

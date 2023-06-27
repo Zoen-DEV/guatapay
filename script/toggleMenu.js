45fcd77a7db200951bbe6cde1accd8d9db805cc5
Enzo Holgado <enzoholgadodev@gmail.com>
2023-06-27 12:10:44 -0500

    fix: refactoring 'toggleMenu.js'

--- script/toggleMenu.js
+++ script/toggleMenu.js
@@ -3,32 +3,32 @@ const menu = document.querySelector(".menu-content");
 const toggleSolutions = document.querySelector(".link-responsive");
 const menuSolutions = document.querySelector(".menu-soluciones-responsive");
 const menuClose = document.querySelector(".menu-close");
-const solutionIcon = document.querySelector(".icon-right-responsive")
+const solutionIcon = document.querySelector(".icon-right-responsive");
 
-toggleMenuBtn.addEventListener("click", (e) => {
-  if (menu.classList.contains("show-menu-content")) {
-    menu.classList.remove("show-menu-content");
-    menuSolutions.classList.remove("show-menu-soluciones");
-    menu.classList.remove("extend-menu-content");
-  } else {
-    menu.classList.add("show-menu-content");
-  }
-});
+const toggleClasses = (element, ...classes) => {
+  classes.forEach((className) => element.classList.toggle(className));
+};
 
-toggleSolutions.addEventListener("click", (e) => {
-  if (menuSolutions.classList.contains("show-menu-soluciones")) {
-    menuSolutions.classList.remove("show-menu-soluciones");
-    menu.classList.remove("extend-menu-content");
-    solutionIcon.classList.remove("rotate-solution-icon")
-  } else {
-    menuSolutions.classList.add("show-menu-soluciones");
-    menu.classList.add("extend-menu-content");
-    solutionIcon.classList.add("rotate-solution-icon")
-  }
-});
+const removeClasses = (element, classes) => {
+  classes.forEach((className) => element.classList.remove(className));
+};
 
-menuClose.addEventListener("click", (e) => {
-    menu.classList.remove("show-menu-content");
-    menuSolutions.classList.remove("show-menu-soluciones");
-    menu.classList.remove("extend-menu-content");
-});
+const handleMenuClick = (e) => {
+  toggleClasses(menu, "show-menu-content");
+  removeClasses(menuSolutions, ["show-menu-soluciones"]);
+};
+
+const handleSolutionsClick = (e) => {
+  toggleClasses(menuSolutions, "show-menu-soluciones");
+  toggleClasses(menu, "extend-menu-content");
+  toggleClasses(solutionIcon, "rotate-solution-icon");
+};
+
+const handleCloseClick = (e) => {
+  removeClasses(menu, ["show-menu-content", "extend-menu-content"]);
+  removeClasses(menuSolutions, ["show-menu-soluciones"]);
+};
+
+toggleMenuBtn.addEventListener("click", handleMenuClick);
+toggleSolutions.addEventListener("click", handleSolutionsClick);
+menuClose.addEventListener("click", handleCloseClick);

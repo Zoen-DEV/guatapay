e844dafbb8befcdef8ccea9dd4e011f6715da84c
Enzo Holgado <enzoholgadodev@gmail.com>
2023-07-05 16:35:48 -0500

    feat: fetch country to set language

--- script/switchLanguage.js
+++ script/switchLanguage.js
@@ -1,7 +1,18 @@
 const buttonsLanguage = document.querySelectorAll(".button-language");
-const lang = navigator.language || navigator.userLanguage;
 const languageFromLocalStorage = window.localStorage.getItem("language");
-const language = languageFromLocalStorage ? languageFromLocalStorage : lang;
+let lang = navigator.language || navigator.userLanguage;
+let language = languageFromLocalStorage ? languageFromLocalStorage : lang;
+
+fetch("http://ip-api.com/json/")
+  .then((res) => res.json())
+  .then((response) => {
+    lang = response.country === "Estados Unidos" ? "en" : "es";
+    language = languageFromLocalStorage ? languageFromLocalStorage : lang;
+    window.localStorage.setItem("language", language);
+  })
+  .catch((data, status) => {
+    console.log("Request country failed");
+  });
 
 // Event listener to switch language buttons
 buttonsLanguage.forEach((btn) => {

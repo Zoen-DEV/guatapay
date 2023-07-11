92ca45da0c8474c704291162c7ad7273896e7390
Enzo Holgado <enzoholgadodev@gmail.com>
2023-07-11 11:40:39 -0500

    fix: change api to get geo location

--- script/switchLanguage.js
+++ script/switchLanguage.js
@@ -3,26 +3,26 @@ const languageFromLocalStorage = window.localStorage.getItem('language')
 const defaultLanguage = navigator.language || navigator.userLanguage || 'es'
 let language = languageFromLocalStorage ? languageFromLocalStorage : defaultLanguage
 
-fetch('http://ip-api.com/json/')
-  .then((res) => res.json())
-  .then((response) => {
-    if (!languageFromLocalStorage) {
-      language = response.countryCode === 'US' ? 'en' : 'es'
+if (!languageFromLocalStorage) {
+  fetch('https://ipgeolocation.abstractapi.com/v1/?api_key=21cb45ae410c4931bedc24656b6d78d7')
+    .then((res) => res.json())
+    .then((response) => {
+      language = response.country_code === 'US' ? 'en' : 'es'
       window.localStorage.setItem('language', language)
-    } else {
-      language = languageFromLocalStorage
-    }
-  })
-  .catch((data, status) => {
-    console.log('Request country failed')
-    if (language !== 'es' || language !== 'en') {
-      if (languageFromLocalStorage) {
-        language = languageFromLocalStorage
-      } else {
-        language = defaultLanguage === 'es' || defaultLanguage === 'en' ? defaultLanguage : 'es'
+    })
+    .catch((data, status) => {
+      console.log('Request country failed')
+      if (language !== 'es' || language !== 'en') {
+        if (languageFromLocalStorage) {
+          language = languageFromLocalStorage
+        } else {
+          language = defaultLanguage === 'es' || defaultLanguage === 'en' ? defaultLanguage : 'es'
+        }
       }
-    }
-  })
+    })
+} else {
+  language = languageFromLocalStorage
+}
 
 // Event listener to switch language buttons
 buttonsLanguage.forEach((btn) => {

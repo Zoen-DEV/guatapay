3cbd75d8fad973065527ec96895431ce50dd5fb7
Enzo Holgado <enzoholgadodev@gmail.com>
2023-07-13 11:40:03 -0500

    fix: correct default language when user first enters the website

--- script/switchLanguage.js
+++ script/switchLanguage.js
@@ -1,28 +1,10 @@
 const buttonsLanguage = document.querySelectorAll('.button-language')
 const languageFromLocalStorage = window.localStorage.getItem('language')
-const defaultLanguage = navigator.language || 'es'
-let language = languageFromLocalStorage ? languageFromLocalStorage : defaultLanguage
-
-if (!languageFromLocalStorage) {
-  fetch('https://ipgeolocation.abstractapi.com/v1/?api_key=21cb45ae410c4931bedc24656b6d78d7')
-    .then((res) => res.json())
-    .then((response) => {
-      language = response.country_code === 'US' ? 'en' : 'es'
-      window.localStorage.setItem('language', language)
-    })
-    .catch((data, status) => {
-      console.log('Request country failed')
-      if (language !== 'es' || language !== 'en') {
-        if (languageFromLocalStorage) {
-          language = languageFromLocalStorage
-        } else {
-          language = defaultLanguage === 'es' || defaultLanguage === 'en' ? defaultLanguage : 'es'
-        }
-      }
-    })
-} else {
-  language = languageFromLocalStorage
+let defaultLanguage = navigator.language === 'en' ? navigator.language : 'es'
+if (navigator.language.includes('en')) {
+  defaultLanguage = 'en'
 }
+let language = languageFromLocalStorage === 'es' || languageFromLocalStorage === 'en' ? languageFromLocalStorage : defaultLanguage
 
 // Event listener to switch language buttons
 buttonsLanguage.forEach((btn) => {

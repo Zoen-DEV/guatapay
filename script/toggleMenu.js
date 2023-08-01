d4003fb4d00cb6f1e76aa1096d33db66089a8811
Martín Fenocchio <fenomartin6@gmail.com>
2023-08-01 12:26:52 -0300

    fix button burger menu splash

--- script/toggleMenu.js
+++ script/toggleMenu.js
@@ -1,22 +1,22 @@
-const toggleMenuBtn = document.querySelector(".menu-button");
-const menu = document.querySelector(".menu-container");
-const menuClose = document.querySelector(".cancel-icon");
+const toggleMenuBtn = document.querySelector('.menu-icon-tigger')
+const menu = document.querySelector('.menu-container')
+const menuClose = document.querySelector('.cancel-icon')
 
 const toggleClasses = (element, ...classes) => {
-  classes.forEach((className) => element.classList.toggle(className));
-};
+  classes.forEach((className) => element.classList.toggle(className))
+}
 
 const removeClasses = (element, ...classes) => {
-  classes.forEach((className) => element.classList.remove(className));
-};
+  classes.forEach((className) => element.classList.remove(className))
+}
 
 const handleMenuClick = (e) => {
-  menu.classList.toggle("show-menu-content");
-};
+  menu.classList.toggle('show-menu-content')
+}
 
 const handleCloseClick = (e) => {
-  menu.classList.remove("show-menu-content");
-};
+  menu.classList.remove('show-menu-content')
+}
 
-toggleMenuBtn.addEventListener("click", handleMenuClick);
-menuClose.addEventListener("click", handleCloseClick);
+toggleMenuBtn.addEventListener('click', handleMenuClick)
+menuClose.addEventListener('click', handleCloseClick)
